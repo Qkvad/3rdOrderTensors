8119c019a2377b2107532c14a752387e80333f5d
Petra Brčić <petrabrcic94@gmail.com>
2018-05-10 10:45:28 +0200

    FIX: tproduct typo

--- src/tproduct_fft.m
+++ src/tproduct_fft.m
@@ -3,7 +3,7 @@ function [C] = tproduct_fft (A, B)
   [p1,m,n1] = size(B);
   assert (p==p1 && n==n1, "Tensor dimensions do not match!");
   A1 = fft(A,[],3);
-  B1 = fft(A,[],3);
+  B1 = fft(B,[],3);
   C1 = zeros(l,m,n);
   for i=1:n
     C1(:,:,i)=A1(:,:,i)*B1(:,:,i);
