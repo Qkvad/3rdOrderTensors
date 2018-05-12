fa9671662148602d0c1925527b6079fb943f054e
Sandro Lovnički <lovnicki.sandro@gmail.com>
2018-05-12 16:04:29 +0200

    src/: [add] fold3, [update] tproduct
    	- tproduct: [use] fold3(circ(A)*matvec(B)) definition if n=3

--- src/tproduct.m
+++ src/tproduct.m
@@ -2,11 +2,16 @@ function [C] = tproduct (A, B)
   [l,p,n] = size(A);
   [p1,m,n1] = size(B);
   assert (p==p1 && n==n1, "Tensor dimensions do not match!");
-  C = zeros(l,m,n);
-  for i=1:l
-    for j=1:m
-      for k=1:p
-        C(i,j,:) += tubalScalarMul(A(i,k,:),B(k,j,:));
+  
+  if n==3
+    C = fold3(circ(A)*matvec(B));
+  else
+    C = zeros(l,m,n);
+    for i=1:l
+      for j=1:m
+        for k=1:p
+          C(i,j,:) += tubalScalarMul(A(i,k,:),B(k,j,:));
+        end
       end
     end
   end
