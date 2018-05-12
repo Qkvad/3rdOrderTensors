4c205dd8acc679bebcf2c84db789d9d73b9f34a9
Sandro Lovnički <lovnicki.sandro@gmail.com>
2018-05-12 16:44:17 +0200

    src/fold3: [fix] loop

--- src/fold3.m
+++ src/fold3.m
@@ -2,9 +2,8 @@ function [fld]=fold3(M)
   [l_n,m] = size(M);
   % this is meant for images, so n==3
   l = l_n/3;
-  % fld=zeros(l,m,3)
   for k=1:3
-    fld(:,:,k) = M(1:l,:);
+    fld(:,:,k) = M((k-1)*l+1:k*l,:);
   end
 end
   
