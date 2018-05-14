3ed54da62bca3b3400047c7c8b960bdddf5b2319
Sandro Lovnički <lovnicki.sandro@gmail.com>
2018-05-14 03:38:13 +0200

    src/twist: [enable] twisting general tensors

--- src/twist.m
+++ src/twist.m
@@ -1,3 +1,12 @@
 function [twst]=twist(M)
-  twst(:,1,:) = M(:,:);
+  % implemented to twist all frontal slices if M is third-order tensor
+  [f,s] = size(size(M));
+  if s==2 % normal matrix twist
+    twst(:,1,:) = M(:,:);
+  else % tensor twist
+    [l,m,n] = size(M);
+    for i=1:n
+      twst(:,i,:) = M(:,:,i);
+    end
+  end  
 end
