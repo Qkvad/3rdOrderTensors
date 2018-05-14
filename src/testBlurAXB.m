d58746676d5fd4d1ebb591ff0d445a6809257121
Sandro Lovnički <lovnicki.sandro@gmail.com>
2018-05-14 08:14:01 +0200

    src/testBlurAXB: [simplify] code

--- src/testBlurAXB.m
+++ src/testBlurAXB.m
@@ -1,8 +1,9 @@
-function [A,B] = testBlurAXB(impath,sigma)
+function testBlurAXB(impath,sigma)
   % load image and convert it to grayscale
   img = imread(impath);
   img = rgb2gray(img);
   [m,n] = size(img); 
+  
   % show image
   figure(1);
   imshow(img);
@@ -11,43 +12,20 @@ function [A,B] = testBlurAXB(impath,sigma)
   % twist image to m x 1 x n shape
   X = twist(img);
   
-  % toeplitz matrices parameters
-  bln = n;
-  zro = n/2;
-  %sigma=2.0;
-  
-  % compute toeplitz matrices with gaussian psf, stack them into tensor
-  max=0; maxi=0;
-  for i = -bln/2:bln/2-1
-   for k = 1:bln
-      c(k) = psf(sigma, i, k-1);
-      r(k) = psf(sigma, i, 1-k);
-   end
-   A(:,:,bln/2+i+1) = toeplitz(c,r);
-   tpln = norm(A(:,:,bln/2+i+1));
-   if tpln > max
-     max = tpln;
-     maxi = bln/2+i+1;
-   end
-  end 
-  % fix some of the important frontal slices
-  % put maximal toeplitz matrix on first frontal slice
-  A(:,:,1) = eye(bln,bln) + A(:,:,maxi);  
-  % set to zero "irrelevant" slices
-  for i=zro+1:n
-    A(:,:,i) = zeros(bln,bln);
-  end
+  % crate tensor A with toeplitz blocks and gaussian point spread function
+  A = createA(n,sigma);
   
   % apply blur to image
   B = tproduct(A,X);
   
-  % squeeze it for show, convert to grayscale
-  imgB = round(squeeze(B));
-  imgB = mat2gray(imgB,[0 255]);
   % show blurred image
-  pause(1);
-  figure(2);
-  imshow(imgB);
+  showTimg(B);
+  
+  % deblurr
+  [X,P,R] = tCG(A,B);
+  
+  % show deblurred image
+  showTimg(X);
   
   
   
