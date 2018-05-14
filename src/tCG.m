c9fdfed6cf248eeda2b416599e1220d4aadf69f3
Sandro Lovnički <lovnicki.sandro@gmail.com>
2018-05-14 08:11:28 +0200

    src/tCG: [update]
    	- break when X is not changing
    	- initial guess all zeros
    	- print interations count

--- src/tCG.m
+++ src/tCG.m
@@ -1,13 +1,16 @@
 function [X,P,R]=tCG(A,B)
   [lA,mA,nA]=size(A);
   if lA==mA
-    X=ones(1,1,nA);
+    X = zeros(lA,1,nA);
     [R,a]=normalize(B);
     P=R;
-    for i=1:10
+    for i=1:lA      
       c=tproduct(tinv(tproduct(tproduct(transp(P),A),P)),tproduct(transp(R),R));
+      Xold = X;    
       X=X+tproduct(P,c);
-      showTimg(X);
+      if norm(squeeze(X-Xold),2) < 0.000001
+        break
+      end     
       Rold=tproduct(transp(R),R);
       R=R-tproduct(A,tproduct(P,c));
       d=tproduct(tinv(Rold),tproduct(transp(R),R));
@@ -16,5 +19,6 @@ function [X,P,R]=tCG(A,B)
   else
     sprintf('Dimensions dont match. \nExpected for arg1: mxmxn \n Expected for arg2: mx1xn');      
   end
+  printf("tCG done in %d iterations.\n", i);
   X=tproduct(X,a);
 end
